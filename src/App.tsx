1f0d1543154c093ae57d73fcd2872a869bfa4dc6
DavidTheProgrammer <18518167+DavidTheProgrammer@users.noreply.github.com>
2022-06-30 01:28:31 +0200

    Created Nav and Router Outlet and paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navbar from "./features/navbar/Navbar";
+import {Outlet} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="app">
-
-        </div>
+        <>
+            <Navbar/>
+            <div className="pt-16">
+                <Outlet/>
+            </div>
+        </>
     );
 }
 
--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,5 +1,41 @@
-const Navbar = () => {
+import {Link, useLocation} from "react-router-dom";
 
+interface NavLink {
+    pathName: string;
+    text: string;
+}
+
+const links: NavLink[] = [
+    {
+        pathName: "/",
+        text: "Popular",
+    },
+    {
+        pathName: "/liked",
+        text: "Liked"
+    },
+]
+
+
+const Navbar = () => {
+    const location = useLocation();
+
+    return (
+        <header className="fixed left-0 right-0 z-5 bg-slate-100 rounded-b-lg h-16 shadow-lg">
+            <nav className="h-full w-full px-8 flex justify-between items-center">
+                <div className="text-2xl font-medium">
+                    Movies
+                </div>
+                <div className="flex text-md">
+                    {links.map(link => (
+                        <div className={`mr-8 py-1 px-4 rounded-b-sm ${link.pathName === location.pathname && "border-b-4 border-blue-500"}`}>
+                            <Link to={link.pathName}>{link.text}</Link>
+                        </div>
+                    ))}
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar;
